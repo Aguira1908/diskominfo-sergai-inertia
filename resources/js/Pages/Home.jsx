0ac6145596270d6c82d514cd47bd3011865d27aa
zikkri amri <zikkriamri123@gmail.com>
2025-04-21 01:20:16 +0700

    feat: install & configure Inertia.js (React) + Filament

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="w-full items-center flex justify-center  ">
+            Setup Inertia
+        </div>
+    );
+};
+
+export default Home;
