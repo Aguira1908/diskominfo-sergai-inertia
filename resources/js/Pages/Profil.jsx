82d17a3902ccf1f20ff0eed2e5e9d1ecf74e343b
zikkri amri <zikkriamri123@gmail.com>
2025-04-26 18:40:06 +0700

    feat(inertia): add profil page

---
+++ resources/js/Pages/Profil.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profil = () => {
+    return (
+        <div className="w-[100vh] h-[100vh] flex justify-center items-center ">
+            Hello World From Profil
+        </div>
+    );
+};
+
+export default Profil;
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+
+
+Route::get('/profil', function () {
+    return Inertia::render('Profil');
+});
+
+
+
+
+
